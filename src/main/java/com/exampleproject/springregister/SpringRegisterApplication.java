97a037118d8db167ce4ca81f03f606a0069a72f3
Robert <robert.hughes@capgemini.com>
2022-04-27 14:36:22 -0700

    CORS config

--- src/main/java/com/exampleproject/springregister/SpringRegisterApplication.java
+++ src/main/java/com/exampleproject/springregister/SpringRegisterApplication.java
@@ -2,6 +2,9 @@ package com.exampleproject.springregister;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringRegisterApplication {
@@ -9,4 +12,15 @@ public class SpringRegisterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRegisterApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/users").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
+
 }
--- src/main/java/com/exampleproject/springregister/controller/UserController.java
+++ src/main/java/com/exampleproject/springregister/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import com.exampleproject.springregister.service.UserService;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
 	private static final Logger logger = LogManager.getLogger(UserController.class);
